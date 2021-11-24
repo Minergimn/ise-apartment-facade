a5449aa2a4d75f11b4f453c92d9b4dacab816432
Viktor Deryabin <vderyabin@aligntech.com>
2021-11-24 21:08:14 +0300

    task-6: simplest kafka client

---
+++ internal/config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"gopkg.in/yaml.v3"
+	"os"
+	"path/filepath"
+)
+
+var cfg *Config
+
+// GetConfigInstance returns service config
+func GetConfigInstance() Config {
+	if cfg != nil {
+		return *cfg
+	}
+
+	return Config{}
+}
+
+// Kafka - contains all parameters kafka information.
+type Kafka struct {
+	Capacity uint64   `yaml:"capacity"`
+	Topic    string   `yaml:"topic"`
+	GroupID  string   `yaml:"groupId"`
+	Brokers  []string `yaml:"brokers"`
+}
+
+// Config - contains all configuration parameters in config package.
+type Config struct {
+	Kafka Kafka `yaml:"kafka"`
+}
+
+// ReadConfigYML - read configurations from file and init instance Config.
+func ReadConfigYML(filePath string) error {
+	if cfg != nil {
+		return nil
+	}
+
+	file, err := os.Open(filepath.Clean(filePath))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = file.Close()
+	}()
+
+	decoder := yaml.NewDecoder(file)
+	if err := decoder.Decode(&cfg); err != nil {
+		return err
+	}
+
+	return nil
+}
